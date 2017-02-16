8e342332f7bd9dfe329a381839a9aaed1b8164f8
hafizdjauhari <hafiz2119@gmail.com>
2017-02-16 14:21:44 +0700

    update

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.implicitintents;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -67,9 +68,11 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickShareTextButton(View v) {
         // TODO (5) Specify a String you'd like to share
-
+        String textThatYouWantToShare =
+                "katanya disuruh nulis ginian sih" +
+                        "check out Udacity and Google's Android Nanodegree!";
         // TODO (6) Replace the Toast with shareText, passing in the String from step 5
-        Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        shareText(textThatYouWantToShare);
     }
 
     /**
@@ -145,6 +148,21 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a void method called shareText that accepts a String as a parameter
     // Do steps 2 - 4 within the shareText method
+    private void shareText(String berbagi){
+
+        String mimeType = "text/plain";
+
+        String title = "belajar cara berbagi";
+
+        ShareCompat.IntentBuilder
+
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(title)
+                .setText(berbagi)
+                .startChooser();
+
+    }
 
         // TODO (2) Create a String variable called mimeType and set it to "text/plain"
 
